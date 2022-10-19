fd16dd4be1472b1de64365999f93f0dd8422490f
NikitMaclin <mnikita167@gmail.com>
2022-10-19 19:22:37 +0300

    Выводит на экран ошибку, деление на ноль пока что не получилось сделать

--- web-страница версия 3.0/php код/kalkulator.php
+++ web-страница версия 3.0/php код/kalkulator.php
@@ -2,39 +2,70 @@
 
     // Калькулятор
 
-
+    
     $otvet = "";
+
+    if (!empty($_REQUEST ["knop"]) && (empty($_REQUEST ["one"]) || empty($_REQUEST ["oper"]) || empty ($_REQUEST["two"]))){
+        $error = "Поля пустые, заполните их и повторите операцию";
+    }
     if (!empty($_REQUEST ["one"]) && !empty($_REQUEST ["oper"]) && !empty ($_REQUEST["two"])){
+        
+        $okno1 = $_REQUEST ["one"];
+        $vvod = $_REQUEST ["oper"];
+        $okno2 = $_REQUEST ["two"];
+
+            if ($vvod == "+"){
+            $otvet = $okno1 + $okno2;
+            }
+
+            if ($vvod == "-"){
+                $otvet = $okno1 - $okno2;
+            }
+
+            if ($vvod == "*"){
+                $otvet = $okno1 * $okno2;
+            }
+
+            if ($vvod == "/"){
+                $otvet = $okno1 / $okno2;
+            }
+
+            if ($okno2 == 0){
+                $blok = "На ноль делить нельзя, введите другое число";
+            }
+
+    }
+
+    /*$otvet = "";
+
+    if (isset($_REQUEST ["knop"]) && (empty($_REQUEST ["one"]) || empty($_REQUEST ["oper"]) || empty ($_REQUEST["two"]))){
+        $error = "Поля пустые, заполните их и повторите операцию";
+    }
+
+    if (isset($_REQUEST ["one"]) && isset($_REQUEST ["two"])){
 
         $okno1 = $_REQUEST ["one"];
         $vvod = $_REQUEST ["oper"];
         $okno2 = $_REQUEST ["two"];
 
-        if ($vvod == "+"){
+            if ($vvod == "+"){
             $otvet = $okno1 + $okno2;
-        }
+            }
 
-        if ($vvod == "-"){
-            $otvet = $okno1 - $okno2;
-        }
+            if ($vvod == "-"){
+                $otvet = $okno1 - $okno2;
+            }
 
-        if ($vvod == "*"){
-            $otvet = $okno1 * $okno2;
-        }
+            if ($vvod == "*"){
+                $otvet = $okno1 * $okno2;
+            }
 
-        if ($vvod == "/"){
-            $otvet = $okno1 / $okno2;
-        }
+            if ($vvod == "/"){
+                $otvet = $okno1 / $okno2;
+            }
+          //  else $error = "На ноль делить нельзя, введите другое число";
 
-        if ($okno2 == 0){
-            $blok = "На ноль делить нельзя, введите другое число";
-        }
-
-        if ($okno1 == "" && $okno2 == ""){
-            $error = "Поля пустые, заполните их и повторите операцию";
-        }
-
-    }
+    }*/
     
     ?>
 
