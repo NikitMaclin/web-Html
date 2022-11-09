e216762c455cabc616ba8bd5132681ec949c97dc
NikitMaclin <mnikita167@gmail.com>
2022-11-09 19:54:02 +0300

    Рабочий калькулятор с ajax

--- web-страница версия 3.0/аякс калькулятор/kalkulator.php
+++ web-страница версия 3.0/аякс калькулятор/kalkulator.php
@@ -1,53 +1,57 @@
 <?php
-    // Калькулятор
     $connecti = mysqli_connect("localhost" , "root" , "" , "kalkulator");
-    if (!$connecti){
+    if (!$connecti) {
         die ("Связь не установлена: " . mysqli_connect_error());
     }
 
-    // $blok = "";
-    // $error = "";
-    if (isset($_REQUEST ["knop"])){
-        if ($_REQUEST ["one"]!="" && $_REQUEST ["oper"]!="" && $_REQUEST["two"]!=""){
+    $resultpost = "";
+    $blok = "";
+    $error = "";
+        if ($_REQUEST ["one"]!="" && $_REQUEST ["oper"]!="" && $_REQUEST["two"]!="") {
 
             $okno1 = $_REQUEST ["one"];
             $vvod = $_REQUEST ["oper"];
             $okno2 = $_REQUEST ["two"];
             
-            if ($vvod == "+"){
+            if ($vvod == "+") {
                 $result = $okno1 + $okno2;
             }
-            if ($vvod == "-"){
+            if ($vvod == "-") {
                 $result = $okno1 - $okno2;
             }
-            if ($vvod == "*"){
+            if ($vvod == "*") {
                 $result = $okno1 * $okno2;
             }
-            if ($vvod == "/"){
-                if ($okno2 <> 0){
+            if ($vvod == "/") {
+                if ($okno2 <> 0) {
                     $result = $okno1 / $okno2;
                 }
                 else {
-                    $blok = "На ноль делить нельзя, введите другое число";
+                    $blok = "Ошибка: На ноль делить нельзя, введите другое число";
                 }
             }
-            mysqli_query($connecti, "INSERT INTO baza_kalcul (one_data, operetion, two_data, otvet) VALUES ('" . $_REQUEST["one"] . "', '" . $_REQUEST ["oper"] . "', '". $_REQUEST["two"] . "', '". $result ."')");
+            if (isset($result)) {
+                mysqli_query ($connecti, "INSERT INTO baza_kalcul (one_data, operetion, two_data, otvet) VALUES ($okno1, '$vvod', $okno2, $result)");
+                $resultpost = "Последний результат = " . $result;
+            }
+            
         }
         else {
-            $error = "Поля пустые, заполните их и повторите операцию";
+            $error= "Ошибка: Поля пустые, заполните их и повторите операцию";
         }
-    }
 
-    $query = mysqli_query($connecti, "SELECT * FROM baza_kalcul WHERE id ORDER BY id DESC LIMIT 7");
+    //$query = mysqli_query ($connecti, "SELECT * FROM baza_kalcul WHERE id ORDER BY id DESC LIMIT 7");
+    $query = mysqli_query ($connecti, "SELECT * FROM baza_kalcul ORDER BY -ID LIMIT 7");
     
 
     $bazatabl = [];
 
-    while ($row = mysqli_fetch_assoc($query)){ //Извлечёт из ответа $query первую строку, кторая в ней находится
+    while ($row = mysqli_fetch_assoc($query)) { //Извлечёт из ответа $query первую строку, кторая в ней находится
         $bazatabl[] = $row; // Добавить в конец массива $morze сгенерированную строку
     }
 
     $vdox = [
+        "resultpost" => $resultpost,
         "blok" => $blok,
         "error" => $error,
         "bazatabl" => $bazatabl
@@ -55,79 +59,3 @@
 
     echo json_encode ($vdox);
 ?>
-
-<!--<html>
-    <head>
-    
-    </head>
-    <body>
-
- Калькулятор 
-        <form method = "post">
-            <div> 
-                <div>Калькулятор</div>
-                <div>Введите первое число:</div>
-                    <input type="number" name = "one" />
-                <div>Выберите операцию:
-                    <div>
-                        <select name ="oper" style="padding-inline-end: 522px; padding-top: 15px;">
-                            <option>+</option>
-                            <option>-</option>
-                            <option>*</option>
-                            <option>/</option>
-                        </select>
-                    </div>
-                </div>
-                <div>Введите второе число:</div>
-                    <input type="number" name = "two"/>
-                
-                <div>
-                    <input type = "submit" name = "knop" value = "Вычислить" />
-                </div>
-            </div>
-        </form>
-
-        </?php if(!empty($blok)) { ?>
-            <div>
-                <span>Ваш результат: </span>
-                <span>
-                    </?php echo $blok; ?>
-                </span>
-            </div>
-        </?php } ?>
-
-        </?php if(!empty($error)) { ?>
-            <div>
-                <span>Ваш результат: </span>
-                <span>
-                    </?php echo $error; ?>
-                </span>
-            </div>
-        </?php } ?>
-
-        </?php if(isset($result)) { ?>
-            <div>
-                <span>Ваш результат: </span>
-                <span>
-                    </?php echo $result; ?>
-                </span>
-            </div>
-        </?php } ?>
-
-        </?php foreach ($bazatabl as $user) {?>
-            <form>
-                <span>-> [</span></?php echo $user["id"];?><span>]</span> 
-                </?php echo $user["one_data"]; ?>
-                </?php echo $user["operetion"]; ?>
-                </?php echo $user["two_data"]; ?> <span> = </span>
-                </?php echo $user["otvet"]; ?>
-                
-                <input type = "hidden" value = "</?php echo $user["id"];?>" name ="user_id"/>
-                <input type = "submit" value = "Удалить" name = "delete"/> 
-            </form>
-            
-        </?php } ?>
-           
-
-    </body>
-</html>-->
